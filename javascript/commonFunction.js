c8b31f292ca294d47f41e680c5168667c506c19d
pritom-roy <pritomroy20062@gmail.com>
2024-09-24 19:35:00 +0600

    main page done

--- javascript/commonFunction.js
+++ javascript/commonFunction.js
@@ -1,3 +1,23 @@
-function hello() {
-    console.log('syrachuse');
+function get(id) {
+    const element = document.getElementById(id);
+    return element;
+}
+
+function validate(amount, money) {
+    if (isNaN(amount)) return "For a successful donation please input a valid amount.";
+    else if (amount < 0) return "Donate amount can not be negative, Try again.";
+    else if (amount === 0) return "Donation amount can not be zero";
+    else if (amount > money) return "You don't have this amount in your account, Please recharge and try again";
+    else return 1;
+}
+
+
+function makeHistoryContent(donatedAmount, head) {
+    const historyItem = document.createElement('div');
+    historyItem.className = "bg-white border rounded-2xl p-4 mb-3";
+    historyItem.innerHTML = `
+        <h1 class = "text-xl font-bold pb-3">${donatedAmount} Tk Donated for "${head.innerText}" Campaign.</h1>
+        <p>Date: ${new Date()}</p> 
+        `
+    return historyItem;
 }
--- javascript/home.js
+++ javascript/home.js
@@ -1,20 +1,121 @@
-const btnDonateNow1 = document.getElementById('btn-donate-now-1');
-const btnDonateNow2 = document.getElementById('btn-donate-now-2');
-const btnDonateNow3 = document.getElementById('btn-donate-now-3');
-const popup = document.getElementById('popup');
-const btnCloseNow = document.getElementById('btn-close-now');
+const donate = get('donation');
+const history = get('history');
+const myMoney = get('myMoney');
 
-function showPopup() {
-    popup.classList.remove('hidden');
-    document.body.classList.add('no-scroll');
-}
+const donationSection = get('donationSection');
+const historySection = get('historySection');
 
-function hidePopup() {
-    popup.classList.add('hidden');
-    document.body.classList.remove('no-scroll');
-}
+const historyContent = get('historyContent');
 
-btnDonateNow1.addEventListener('click', showPopup);
-btnDonateNow2.addEventListener('click', showPopup);
-btnDonateNow3.addEventListener('click', showPopup);
-btnCloseNow.addEventListener('click', hidePopup);
+const noakhaliMoney = get('noakhaliMoney')
+const amount1 = get('amount1');
+const feniMoney = get('feniMoney');
+const amount2 = get('amount2');
+const supportMoney = get('supportMoney');
+const amount3 = get('amount3');
+
+const btnDonateNow1 = get('btn-donate-now-1');
+const btnDonateNow2 = get('btn-donate-now-2');
+const btnDonateNow3 = get('btn-donate-now-3');
+
+const head1 = get('head1');
+const head2 = get('head2');
+const head3 = get('head3');
+
+const modal = get('my_modal_5');
+
+history.addEventListener('click', function () {
+    history.classList.add("bg-[#B4F461]", "font-bold");
+    donate.classList.remove("bg-[#B4F461]", "font-bold");
+
+    donationSection.classList.add("hidden");
+    historySection.classList.remove("hidden");
+
+});
+
+donate.addEventListener('click', function () {
+    donate.classList.add("bg-[#B4F461]", "font-bold");
+    history.classList.remove("bg-[#B4F461]", "font-bold");
+
+    donationSection.classList.remove("hidden");
+    historySection.classList.add("hidden");
+});
+
+let money = parseFloat(myMoney.innerText);
+
+btnDonateNow1.addEventListener('click', function (event) {
+    event.preventDefault();
+    const donatedAmount = parseFloat(amount1.value);
+    let noah = parseFloat(noakhaliMoney.innerText);
+
+    let result = validate(donatedAmount, money)
+    if (result === 1) {
+        noah += donatedAmount;
+        money -= donatedAmount;
+        myMoney.innerText = money;
+        noakhaliMoney.innerText = noah;
+
+        modal.showModal();
+
+        const he = makeHistoryContent(donatedAmount, head1);
+        historyContent.appendChild(he);
+    }
+    else {
+        alert(result);
+    }
+    amount1.value = '';
+    amount1.focus();
+})
+
+btnDonateNow2.addEventListener('click', function (event) {
+    event.preventDefault();
+    const donatedAmount = parseFloat(amount2.value);
+    let feni = parseFloat(feniMoney.innerText);
+
+    let result = validate(donatedAmount, money)
+    if (result === 1) {
+        feni += donatedAmount;
+        money -= donatedAmount;
+        myMoney.innerText = money;
+        feniMoney.innerText = feni;
+
+        modal.showModal();
+
+        const he = makeHistoryContent(donatedAmount, head2);
+        historyContent.appendChild(he);
+    }
+    else {
+        alert(result);
+    }
+    amount2.value = '';
+    amount2.focus();
+})
+
+btnDonateNow3.addEventListener('click', function (event) {
+    event.preventDefault();
+    const donatedAmount = parseFloat(amount3.value);
+    let support = parseFloat(supportMoney.innerText);
+
+    let result = validate(donatedAmount, money)
+    if (result === 1) {
+        support += donatedAmount;
+        money -= donatedAmount;
+        myMoney.innerText = money;
+        supportMoney.innerText = support;
+
+        modal.showModal();
+
+        const he = makeHistoryContent(donatedAmount, head3);
+        historyContent.appendChild(he);
+    }
+    else {
+        alert(result);
+    }
+    amount3.value = '';
+    amount3.focus();
+})
+
+
+// btnDonateNow1.addEventListener('click', function () {
+//     modal.showModal();
+// });
